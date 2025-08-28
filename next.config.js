cfcf49d27948c8b7fd61f6fba28f8e833a8fc08e
leosmith29 <leosmith2929@yahoo.com>
2025-08-28 03:55:26 +0100

    patches6

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    esmExternals: "loose", // helps with modern ESM deps
+  },
+  webpack(config) {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
   reactStrictMode: true,
   trailingSlash: true,
 };
